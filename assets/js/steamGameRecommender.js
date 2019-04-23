74a895b9cc2bdfd7ca45671153790b6f129ecbdf
byr_xt <hfaltgg@gmail.com>
2019-04-23 16:18:05 -0400

    res-region create

--- assets/js/steamGameRecommender.js
+++ assets/js/steamGameRecommender.js
@@ -1,9 +1,14 @@
 <script>
-a = [0, 5, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+var selected = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var result = [0, 0, 0];
+var selectedNum = 0;
+var imgSrc = ["image/dota2.jpg", "image/csgo.jpg", "image/skyrim.jpg", "image/cod.jpg", "image/payday2.jpg", "image/gtav.jpg", "image/witcher3.jpg", "image/rainbow6.jpg", "image/divididon.jpg", "image/darksoul.jpg", "image/fallout4.jpg", "image/Resident Evil 7.jpg", "image/pubg.jpg", "image/moh.jpg", "image/origins.jpg", "image/odyssey.jpg", "image/sekiro.jpg", "image/2k19.jpg", "image/hitman2.jpg", "image/RESIDENT EVIL 2.jpg"];
+
 function displayDate1(){
 	document.getElementById("demo").innerHTML=a[1];
 }
+
 function displayDate2(){
-	a[1] = 0;
 }
+
 </script>
