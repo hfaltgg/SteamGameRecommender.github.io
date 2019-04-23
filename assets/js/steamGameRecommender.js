9f071705d59332aaad7d279cea66d466d2c7b9a9
byr_xt <hfaltgg@gmail.com>
2019-04-23 15:31:26 -0400

    create my js

---
+++ assets/js/steamGameRecommender.js
@@ -0,0 +1,9 @@
+<script>
+a = [0, 5, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+function displayDate1(){
+	document.getElementById("demo").innerHTML=a[1];
+}
+function displayDate2(){
+	a[1] = 0;
+}
+</script>
